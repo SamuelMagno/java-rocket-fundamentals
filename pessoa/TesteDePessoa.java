74ecb2d7d6a385cc5c8de6fea1d49be483b47f42
Samuel Magno <samuelrtfmagno@gmail.com>
2024-04-06 19:43:46 -0300

    herança

--- pessoa/TesteDePessoa.java
+++ pessoa/TesteDePessoa.java
@@ -2,21 +2,29 @@ package pessoa;
 
 public class TesteDePessoa {
     public static void main(String[] args) {
-        Pessoa pessoa1 = new Pessoa();
-
-        pessoa1.cpf = "33344455578";
-        pessoa1.nome = "Samuel";
-        pessoa1.idade = 26;
-
-        System.out.println(pessoa1.cpf);
-        System.out.println(pessoa1.nome);
-        System.out.println(pessoa1.idade);
-
         Pessoa p2 = new Pessoa();
 
-        p2.cpf = "43256790832";
-        p2.idade = 28;
-        p2.nome = "Julia";
+        p2.setCpf("43256790832");
+        p2.setIdade(26);
+        p2.setNome("Samuel");
         System.out.println(p2.imprimirDados());
+
+        Professor p3 = new Professor();
+        //Pessoa p4 = new Professor(); Forma válida
+
+        p3.setCpf("12345678911");
+        p3.setIdade(30);
+        p3.setNome("João");
+        p3.setSalario(2000);
+        System.out.println(p3.imprimirDados());
+
+        Aluno p5 = new Aluno();
+        //Pessoa p4 = new Professor(); Forma válida
+
+        p5.setCpf("98765432110");
+        p5.setIdade(21);
+        p5.setNome("José");
+        p5.setMatricula("abc98");
+        System.out.println(p5.imprimirDados());
     }
 }
