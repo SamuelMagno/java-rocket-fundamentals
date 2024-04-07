cc9cfe3f65220d790e36ec84075a17ff42ab4ab7
Samuel Magno <samuelrtfmagno@gmail.com>
2024-04-07 17:26:14 -0300

    wrapper classes

---
+++ wrapper/Wrapper.java
@@ -0,0 +1,17 @@
+package wrapper;
+
+public class Wrapper {
+    public static void main(String[] args) {
+        int tipoInt = 1;
+        float tipoFloat = 1;
+        boolean tipoBoll = true;
+
+        Float wFloat = 1F;
+
+        Boolean wBoll = false;
+
+        Integer wInt = 1;
+
+        int a = wInt.compareTo(1);
+    }
+}
