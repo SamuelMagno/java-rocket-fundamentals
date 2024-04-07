3c53c89a706d88f0b45211cc21b18c59788d9761
Samuel Magno <samuelrtfmagno@gmail.com>
2024-04-07 16:48:40 -0300

    construct

---
+++ construtor/Construtor.java
@@ -0,0 +1,17 @@
+package construtor;
+
+public class Construtor {
+    private int numero;
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public Construtor(int numero) {
+        this.numero = numero;
+    }
+}
---
+++ construtor/TesteConstrutor.java
@@ -0,0 +1,9 @@
+package construtor;
+
+public class TesteConstrutor {
+    public static void main(String[] args) {
+        Construtor c = new Construtor(10);
+
+        System.out.println(c.getNumero());
+    }
+}
